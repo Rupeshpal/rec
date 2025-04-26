17f7a2d31b548de2f7f1a671d5114ce903bfab65
Rupesh <palrupesh292@gmail.com>
2025-04-26 14:19:15 +0545

    Added API folder for handling OPD and other API requests

---
+++ src/Components/Autologout.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const AutoLogout = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const token = Cookies.get("authToken");
+      if (!token && location.pathname !== "/Login") {
+        console.log("Token expired or removed, redirecting to login...");
+        localStorage.removeItem("authtoken");
+        navigate("/Login");
+      }
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [navigate, location]);
+
+  return null;
+};
+
+export default AutoLogout;
